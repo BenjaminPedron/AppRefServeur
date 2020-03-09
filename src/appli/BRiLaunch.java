a36f9c2edbae379e7fb25befb62bbd067f9e672c
benjaminGUYT <55363637+benjaminGUYT@users.noreply.github.com>
2020-03-09 23:48:36 +0100

    Mettre en forme serviceRegistery

--- src/appli/BRiLaunch.java
+++ src/appli/BRiLaunch.java
@@ -4,10 +4,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Scanner;
+import java.net.Socket;
+import javax.tools.JavaCompiler;
 
 import bri.ServeurBRi;
 import bri.Service;
 import bri.ServiceRegistry;
+import services.ServiceInversion;
 
 public class BRiLaunch {
 	private final static int PORT_SERVICE = 3000;
@@ -23,6 +26,7 @@ public class BRiLaunch {
 		System.out.println("Pour ajouter une activité, celle-ci doit être présente sur votre serveur ftp");
 		System.out.println("A tout instant, en tapant le nom de la classe, vous pouvez l'intégrer");
 		System.out.println("Les clients se connectent au serveur 3000 pour lancer une activité");
+		System.out.println("Les developpers se connectent au serveur 3001 pour gérer une activité");
 		
 		new Thread(new ServeurBRi(PORT_SERVICE)).start();
 		
@@ -32,12 +36,22 @@ public class BRiLaunch {
 					URL[] classLoaderUrls = new URL[]{new URL("ftp://localhost:2121/")};
 			        
 			        /* Create a new URLClassLoader  */
-			        URLClassLoader urlClassLoader = new URLClassLoader(classLoaderUrls);
-			        Class<? extends Service> servCl = (Class<? extends Service>) urlClassLoader.loadClass("examples."+classeName);
-			        ServiceRegistry.addService(servCl);
+					URLClassLoader urlClassLoader = new URLClassLoader(classLoaderUrls);
+					
+					//String tmp[] = classeName.split(".");
+					//String tmps = tmp[tmp.length];
+
+					//ServiceInversion si = new ServiceInversion(null);
+
+					//if(tmps.equals("java")) {
+					//}
+					//else {
+						Class<? extends Service> servCl = (Class<? extends Service>) urlClassLoader.loadClass("examples." + classeName);
+						ServiceRegistry.addService(servCl, classeName);
+					//}
 				} catch (Exception e) {
 					System.out.println(e);
 				}
-			}		
+			}
 	}
 }
--- src/bri/ServiceRegistry.java
+++ src/bri/ServiceRegistry.java
@@ -4,10 +4,12 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 import java.util.List;
 import java.util.Vector;
+import java.lang.reflect.*;
+import java.net.Socket;
 
 public class ServiceRegistry {
 	// cette classe est un registre de services
-	// partagée en concurrence par les clients et les "ajouteurs" de services,
+	// partagï¿½e en concurrence par les clients et les "ajouteurs" de services,
 	// un Vector pour cette gestion est pratique
 
 	static {
@@ -15,35 +17,105 @@ public class ServiceRegistry {
 	}
 	private static Vector<Class<? extends Service>> servicesClasses;
 
-// ajoute une classe de service après contrôle de la norme BLTi
-	public static void addService(Class<? extends Service> classe) throws Exception {
-		// vérifier la conformité par introspection
-		if (!checkIfClassImplements(classe, bri.Service.class))
-			throw new Exception("Le service doit implementer" + bri.Service.class);
-		if (!Modifier.isPublic(classe.getModifiers()))
-			throw new Exception("Votre classe doit être public !");
-		if (Modifier.isAbstract(classe.getModifiers()))
-			throw new Exception("Votre classe doit être abstract !");
-		// ajout du service
+// ajoute une classe de service aprÃ¨s contrÃ´le de la norme BLTi
+
+public static void addService(Class<? extends Service> classe, String classeName) throws Exception {
+	if (contains(classe))
+		throw new Exception("Vous ne pouvez ajoutez deux fois le mÃªme service");
+	if (normeBri(classe))
 		servicesClasses.add(classe);
-	}
+	System.out.println("size :" + servicesClasses.get(0).getSimpleName());
+}
 
 	// renvoie la classe de service (numService -1)	
 	public static Class<? extends Service> getServiceClass(int numService) {
 		return servicesClasses.get(numService - 1);
 	}
 
-// liste les activités présentes
+	// liste les activitï¿½s prï¿½sentes
 	public static String toStringue() {
-		return "toto";
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < servicesClasses.size(); i++) {
+			synchronized (servicesClasses) {
+				sb.append(servicesClasses.get(i).getSimpleName() + " [" + (i + 1) + "] | ");
+			}
+		}
+		return sb.toString();
 	}
 
-	private static boolean checkIfClassImplements(Class<?> classe1, Class<?> classe2) {
-		for (Class<?> i : classe1.getInterfaces()) {
-			if (i.equals(classe2))
-				return true;
+
+
+	private static boolean contains(Class<?> service) {
+		for (Class<? extends Service> class1 : servicesClasses) {
+			if (class1.getName().equals(service.getName()))
+				;
+			return true;
 		}
 		return false;
 	}
 
+	// NORME BRI
+	private static boolean normeBri(Class<?> service) throws Exception {
+		return implementsServiceInterface(service) && isPublic(service) && isNotAbstract(service)
+				&& hasPublicConstructorWithoutException(service) && hasSocketAttributeFinal(service)
+				&& hasStaticToStringueWithoutException(service);
+
+	}
+
+	private static boolean implementsServiceInterface(Class<?> classe) throws Exception {
+		for (Class<?> c : classe.getInterfaces()) {
+			if (c.getSimpleName().equals("Service")) {
+				return true;
+			}
+		}
+		throw new Exception("Implementes pas interface Service");
+	}
+
+	private static boolean isPublic(Class<?> classe) throws Exception {
+		if (Modifier.isPublic(classe.getClass().getModifiers())) {
+			return true;
+		} else {
+			throw new Exception("La classe n'est pas publique");
+		}
+	}
+
+	private static boolean isNotAbstract(Class<?> classe) throws Exception {
+		if (!Modifier.isAbstract(classe.getModifiers())) {
+			return true;
+		} else {
+			throw new Exception("La classe ne doit pas Ãªtre abstract");
+		}
+	}
+
+	private static boolean hasPublicConstructorWithoutException(Class<?> classe) throws Exception {
+		for (Constructor<?> c : classe.getConstructors()) {
+			if (c.getExceptionTypes().length == 0 && Modifier.isPublic(c.getModifiers())) {
+				return true;
+			}
+		}
+		throw new Exception("Pas de public constructeur sans exception");
+	}
+
+	private static boolean hasSocketAttributeFinal(Class<?> classe) throws Exception {
+		for (Field f : classe.getDeclaredFields()) {
+			if (f.getType().isAssignableFrom(Socket.class)) {
+				if (Modifier.isFinal(f.getModifiers())) {
+					if (Modifier.isPrivate(f.getModifiers())) {
+						return true;
+					}
+				}
+			}
+		}
+		throw new Exception("Pas de socket conforme");
+	}
+
+	private static boolean hasStaticToStringueWithoutException(Class<?> classe) throws Exception {
+		for (Method m : classe.getMethods()) {
+			if (Modifier.isStatic(m.getModifiers()) && m.getName().equals("toStringue")
+					&& m.getAnnotatedExceptionTypes().length == 0) {
+				return true;
+			}
+		}
+		throw new Exception("Pas de toStringue static sans exception");
+	}
 }
